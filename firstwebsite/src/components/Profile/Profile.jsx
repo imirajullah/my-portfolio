1be14ed0b7c69ee68c2dba8a261d1ebe94c390d8
Mirajullah <mirajullah95@gmail.com>
2025-07-25 17:08:22 +0500

    profile.jsx

--- firstwebsite/src/components/Profile/Profile.jsx
+++ firstwebsite/src/components/Profile/Profile.jsx
@@ -9,9 +9,10 @@ const Profile = () => {
         <p><strong>Name:</strong> Miraj Ullah</p>
         <p><strong>Email:</strong> mirajullah95@gmail.com</p>
         <p><strong>Linkedin:</strong> https://www.linkedin.com/in/miraj-ullah-1364ba338?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app </p>
-        <p><strong>Phone:</strong> 0302-5726233</p>
+        <p><strong>Phone:</strong> 0302-5726323</p>
         <p><strong>Location:</strong> Samar Bagh, Dir Lower, Pakistan</p>
         <p><strong>Role:</strong> Junior Web Developer</p>
+        <p><strong>Role:</strong> Blogger</p>
         <p><strong>Education:</strong> BS Software engineering (Riphah International University)</p>
         <p><strong>semester</strong> 5th and <strong>CGPA</strong> is 3.87</p>
       </div>
